7ed446c6c94e484e1ebcd498b463f5043d99c560
= <=>
2025-04-04 16:38:12 +0530

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(SettingTableSeeder::class);
-        $this->call(CouponSeeder::class);
+    //     $this->call(SettingTableSeeder::class);
+    //     $this->call(CouponSeeder::class);
     }
 }
